072425eb6a1778b560d96adc3e7fcd71ddf7bad1
ChienHsin-Chan <hcchien@gmail.com>
2016-05-24 17:41:43 +0800

    update the rss cron

--- rss.pl
+++ rss.pl
@@ -19,20 +19,24 @@ $rss->image(
     link => "https://www.twreporter.org/",
 );
 
-my $api = decode_json(get('http://localhost:8080/posts?where={"state":"published"}&max_results=100&sort=-publishDate'));
+my $api = decode_json(get('http://api-v2.twreporter.org:8080/posts?max_results=100&sort=-publishedDate'));
 for (@{ $api->{_items} }) {
     # $_->{story_link} =~ s/\s//g;
     $_->{title} =~ s/[\x00-\x19]//g; # strip control characters
+    my $byline = '';
+    for my $writer_ref (@{ $_->{writers} }) {
+        $byline .= $writer_ref->{name}
+    }
     $rss->add_item(
         title => $_->{title},
-        description => $_->{og_description} || $_->{content}->{brief},
-        permaLink => ($_->{slug} or next),
-        link => ($_->{slug} or next),
+        description => $_->{og_description} || $_->{content}->{brief}->{draft}->{apiData}->[0]->{content}->[0] || '',
+        permaLink => ('https://www.twreporter.org/a/'.$_->{slug} or next),
+        link => ('https://www.twreporter.org/a/'.$_->{slug} or next),
+        enclosure => { url => ( $_->{heroImage}->{image}->{url} or '' ), type => ( $_->{heroImage}->{image}->{filetype} or '' ), length => ( $_->{heroImage}->{image}->{size} or '' ) },
         dc => {
-            creator => ($_->{byline} or "報導者"),
+            creator => ($byline or "報導者"),
         },
-        pubDate => strftime("%a, %d %b %Y %H:%M:%S %z", localtime($_->{publishDate})),
+        pubDate => strftime("%a, %d %b %Y %H:%M:%S %z", localtime($_->{publishedDate})),
     );
-    print "foo";
 }
 $rss->save("/tmp/twreporters/articles/rss2.xml");
